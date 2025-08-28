c89831f05d46477e0136fef7155c1ed6dd2c12c7
albo01tlive <begins350146@gmail.com>
2025-08-28 17:18:46 +0600

    Добавлен API-заглушка для стрима

--- src/pages/api/stream.js
+++ src/pages/api/stream.js
@@ -1,3 +1,4 @@
+// Пример API-заглушки
 export default function handler(req, res) {
-  res.status(200).json({ message: "API работает" })
+  res.status(200).json({ message: "Stream API работает! Реализуйте обработку донатов и сообщений." });
 }
